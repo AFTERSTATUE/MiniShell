3aa07aab9875b65f7f4c2f128189435acf1e90c3
cailibur <2037397332@qq.com>
2026-07-16 13:40:38 +0800

    delete build

--- minishell/build/CMakeFiles/minishell.dir/src/main.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/minishell.dir/src/main.c.o: \
- /home/cailibur/文档/Cscript/MiniShell/MiniShell/minishell/src/main.c \
- /usr/include/stdc-predef.h \
- /home/cailibur/文档/Cscript/MiniShell/MiniShell/minishell/include/shell.h
